d9c3d99e704d0f7a89a2ab05776d83159bcacffc
Aunteek Naser <aunteek.naser@gmail.com>
2017-02-04 00:15:24 -0600

    Update ChatClient.java

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -5,13 +5,16 @@ import java.net.*;
 import java.io.*;
 import java.awt.*;
 public class ChatClient extends Frame implements Runnable {
+    //what are all of these protected from, why are they protected?
     protected DataInputStream i;
     protected DataOutputStream o;
     protected TextArea output;
     protected TextField input;
     protected Thread listener;
     public ChatClient (String title, InputStream i, OutputStream o) {
+        //what is super? what is title?
         super (title);
+        //what is a buffered input stream?
         this.i = new DataInputStream (new BufferedInputStream(i));
         this.o = new DataOutputStream(new BufferedOutputStream(o));
         setLayout(new BorderLayout());
@@ -19,17 +22,22 @@ public class ChatClient extends Frame implements Runnable {
         output.setEditable(false);
         add ("South", input = new TextField());
         pack ();
+        //this is deprecated, does that mean it can't run, and is there a replacement that does the same thing?
         show ();
+        //what does requestFocus do?
         input.requestFocus();
+        //what is a thread? Is it a single chat?
         listener = new Thread (this);
         listener.start();
     }
     public void run () {
+        //what does try do?
         try {
             while (true) {
                 String line = i.readUTF();
                 output.appendText(line + "\n");
             }
+        //what does catch do?
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -40,6 +48,7 @@ public class ChatClient extends Frame implements Runnable {
                 o.writeUTF((String) e.arg);
                 o.flush();
             } catch (IOException ex) {
+                //what does printStackTrace do?
                 ex.printStackTrace();
                 listener.stop();
             }
@@ -51,11 +60,15 @@ public class ChatClient extends Frame implements Runnable {
             hide();
             return true;
         }
+        //what does the super thing do?
         return super.handleEvent (e);
     }
+    //what is an IOException and what does it mean to throw it? Also does it matter where the array brackets are in the String args section?
     public static void main (String args[]) throws IOException {
         if (args.length != 2)
+            //why is there no brackets? And what is a runtime exception?
             throw new RuntimeException("Syntax: ChatClient <host> <port>");
+        //what does parseInt do and why is it relevant?
         Socket s = new Socket (args[0], Integer.parseInt (args[i]));
         new ChatClient ("Chat " + args[0] + ":" + args[1], s.getInputStream(), s.getOutputStream());
     }
